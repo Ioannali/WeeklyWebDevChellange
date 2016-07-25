a2f2dbfcc859e99f24b623b49a529619797d3d3d
Ioannali <jost@op.pl>
2016-07-25 19:41:44 +0200

    angular challenge

---
+++ Angular challenge/js/app.js
@@ -0,0 +1,12 @@
+
+(function(){
+var app = angular.module('store',[]);
+app.controller("StoreController", function(){
+  this.product = gem;
+});
+var gem = {
+  name: "oko",
+  price: 2.83,
+  desc: "dd ff ss"
+}
+})();
